2af400509be8beea261c44d18372f30473644fd0
gabrielsaruhashi <gabriel.saruhashi@gmail.com>
2020-08-12 10:33:22 -0300

    Updates

---
+++ precache-manifest.1d7e38f2e43b83b994a468463a788c52.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467605d491a00a422bd93191469c9e30",
+    "url": "/letters-organizations-client/index.html"
+  },
+  {
+    "revision": "1096b5f08babaa350af6",
+    "url": "/letters-organizations-client/static/css/2.c0155d99.chunk.css"
+  },
+  {
+    "revision": "59511e24baad0ae7e652",
+    "url": "/letters-organizations-client/static/css/main.f9a61b9f.chunk.css"
+  },
+  {
+    "revision": "1096b5f08babaa350af6",
+    "url": "/letters-organizations-client/static/js/2.369ca243.chunk.js"
+  },
+  {
+    "revision": "cd49a31f8c2399c78777cdf833721914",
+    "url": "/letters-organizations-client/static/js/2.369ca243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59511e24baad0ae7e652",
+    "url": "/letters-organizations-client/static/js/main.86f77b21.chunk.js"
+  },
+  {
+    "revision": "3144ea9adc52da24ad4d",
+    "url": "/letters-organizations-client/static/js/runtime-main.a64d75bd.js"
+  },
+  {
+    "revision": "78e81582f9fbbf4857433c5a3a204cff",
+    "url": "/letters-organizations-client/static/media/bald.78e81582.svg"
+  },
+  {
+    "revision": "2c00f638f1b3fc7253cedc1d7a0aa0b7",
+    "url": "/letters-organizations-client/static/media/investigator.2c00f638.svg"
+  },
+  {
+    "revision": "dfd0363de5257cd27a44cf6bbb31f1a7",
+    "url": "/letters-organizations-client/static/media/logo.dfd0363d.svg"
+  },
+  {
+    "revision": "fdf672c3d9abff3c85af138701a6942b",
+    "url": "/letters-organizations-client/static/media/professor.fdf672c3.svg"
+  },
+  {
+    "revision": "12299a71a673a9b8438aa55f30172e81",
+    "url": "/letters-organizations-client/static/media/woman.12299a71.svg"
+  }
+]);
