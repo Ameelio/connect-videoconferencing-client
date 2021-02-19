4a3470d4832a67b74eaea8ebc6ca66d99cfff32c
gabrielsaruhashi <gabriel.saruhashi@gmail.com>
2021-02-19 10:52:47 +0000

    Deploying to gh-pages from  @ 7c4f40a754d61cd7dad83c85c417d45faf89004b 🚀

---
+++ precache-manifest.3c2107e519b6b201f3a331e93a418e05.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0af94dbcb359ad54d4c4664b3e8c4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83b849034126a2593758",
+    "url": "/static/css/2.41e3ba18.chunk.css"
+  },
+  {
+    "revision": "24f4c07e1f41a9498845",
+    "url": "/static/css/main.9a53b5e4.chunk.css"
+  },
+  {
+    "revision": "83b849034126a2593758",
+    "url": "/static/js/2.f798ec79.chunk.js"
+  },
+  {
+    "revision": "33acd5f2ca5f63702e493709576eac03",
+    "url": "/static/js/2.f798ec79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f4c07e1f41a9498845",
+    "url": "/static/js/main.274a038e.chunk.js"
+  },
+  {
+    "revision": "4019907af3f6705d1755",
+    "url": "/static/js/runtime-main.bef88823.js"
+  },
+  {
+    "revision": "dfd0363de5257cd27a44cf6bbb31f1a7",
+    "url": "/static/media/logo.dfd0363d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a956c10dfc9a89b569cd208324b22580.js"
+  "/precache-manifest.3c2107e519b6b201f3a331e93a418e05.js"
 );
 
 self.addEventListener('message', (event) => {
